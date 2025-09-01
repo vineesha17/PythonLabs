f7eeb9dbd6b63bf6cbd79d9c3ee63c3cedbfa4fe
Vineesha Madnani <vineeshamadnani5@gmail.com>
2025-09-01 15:43:36 +0530

    Create Lab5.py

---
+++ Lab5.py
@@ -0,0 +1,27 @@
+# a. 
+t = (1, 2, 3, 2, 4, 2)
+print("a) Count of 2:", t.count(2))
+
+# b. 
+t = (10, 20, 30)
+print("b) Is 20 in tuple?:", 20 in t)
+
+# c. 
+t = ('H', 'e', 'l', 'l', 'o')
+print("c) Tuple to String:", ''.join(t))
+
+# d. 
+t = (5, 2, 9, 1)
+print("d) Max:", max(t), " Min:", min(t))
+
+# e. 
+t = ("Python", "is", "fun")
+print("e) Tuple of Strings → Single String:", ' '.join(t))
+
+# f.
+t = (5, 1, 4, 2, 3)
+print("f) Sorted Tuple:", tuple(sorted(t)))
+
+# g. 
+t = (10, 20, 30, 40)
+print("g) First Element:", t[0], " Last Element:", t[-1])
